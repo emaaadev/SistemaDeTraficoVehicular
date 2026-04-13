bfd8ed10aed6e19ef30f92cb3c2b05677ca2ded0
WandrysFG <wandrysf@gmail.com>
2026-04-13 00:05:55 -0400

    Co-authored-by-<WandrysFerrand>Ticket#2
    test: se agregan pruebas unitarias del modulo de simulacion

---
+++ tests/TestsTraficoVehicular/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimulacionDeTraficoVehicularAPP.Models;
+
+namespace TestsTraficoVehicular
+{
+    public class VehiculoTests
+    {
+        [Fact]
+        public void Vehiculo_SeMueve_CuandoViaEstaLibre()
+        {
+            var vehiculo = new Vehiculo(1, "Auto");
+            var posicionInicial = vehiculo.Posicion;
+
+            vehiculo.Mover("Via libre");
+
+            Assert.NotEqual(posicionInicial, vehiculo.Posicion);
+        }
+
+        [Fact]
+        public void Vehiculo_NoSeMueve_CuandoSeDetiene()
+        {
+            var vehiculo = new Vehiculo(2, "Bus");
+            var posicionInicial = vehiculo.Posicion;
+
+            vehiculo.Detener("Semaforo en rojo");
+
+            Assert.Equal(posicionInicial, vehiculo.Posicion);
+        }
+    }
+}
